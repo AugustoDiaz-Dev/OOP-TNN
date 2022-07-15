793dedf52e9c083d43962064d05c3e8a8cafad72
Augusto <augustodiaz_1@hotmail.com>
2022-07-15 23:29:20 +1000

    Update an object

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// How to create a simple Object?
+// How to update an Object?
 
 const userOne = {
     // Properties
@@ -12,6 +12,14 @@ const userOne = {
         console.log(`${this.email} has logged out.`);
     }
 }
+
+userOne.name = 'John Lennon'; // Update a value with dot notation
+userOne['email'] = 'johnlennon@mail.ru' // Update a value with bracket notation
+
+userOne.age = 81; // Add a new property
+userOne.logInfo = function () {
+    console.log('Adding information about the user')
+} // Add a new method
 
 console.log(userOne.email);
 console.log(userOne.name);
