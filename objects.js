4d7b1a2a57db52d113e4ff1501df7cb5412c4b1d
Augusto <augustodiaz_1@hotmail.com>
2022-07-16 00:40:47 +1000

    Adding properties and methods

--- objects.js
+++ objects.js
@@ -1,9 +1,17 @@
-// Create a class 
+// Adding properties and methods to a class
 
 class User {
+    // In the constructor, we can set the properties of the object
     constructor(email, name) {
         this.email = email;
         this.name = name;
+    }
+    // We can also add methods to the class, outside of the constructor
+    login() {
+        console.log(`${this.email} has logged in`);
+    }
+    logout() {
+        console.log(`${this.email} has logged out`);
     }
 }
 
@@ -12,12 +20,5 @@ const user2 = new User('ringostarr@gmail.com', 'Ringo Starr');
 const user3 = new User('georgeharrison@gmail.com', 'George Harrison');
 const user4 = new User('johnlennon@gmail.com', 'John Lennon');
 
-console.log(user1);
-console.log(user2);
-console.log(user3);
-console.log(user4);
-
-// The 'new' keyword.
-// It creates a new empty object {}
-// It sets the value of 'this' to be the new empty object
-// It calls the constructor method
+user1.login();
+user2.logout();
